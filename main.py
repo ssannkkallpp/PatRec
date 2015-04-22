9d295ee8dfcf94f9545c4b13ccec81660bb5104d
ssannkkallpp <ssannkkallpp@gmail.com>
2015-04-22 19:21:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from numpy import *
+from itertools import *
+import itertools
+def compare(a,b):
+    x=[max(a[i],b[i])-min(a[i],b[i]) for i in range(0,len(a))]
+    return sum(x), b
+def init(x):
+    img = Image.open(str(x) + '.png')
+    arr = array(img)
+    new_arr=list(itertools.chain(*arr)) # flattens arr into a 1d array
+    # example -> [[3,4],[2,3]] -> [3,4,2,3]
+    av_arr=[list(i) for i in new_arr]
+    av_arr=[sum(x)/len(x) for x in av_arr]
+    return av_arr
+def process(x):
+    img = Image.open(x)
+    arr = array(img)
+    new_arr=list(itertools.chain(*arr)) # flattens arr into a 1d array
+    # example -> [[3,4],[2,3]] -> [3,4,2,3]
+    av_arr=[list(i) for i in new_arr]
+    av_arr=[sum(x)/len(x) for x in av_arr]
+    return av_arr
+def main(input_file):
+    arr=[init(i) for i in range(0,10)]
+    in_arr=process(input_file)
+    minimum=compare(in_arr,arr[0])
+    for x in arr:
+        if compare(in_arr, x)[0] < minimum[0]:
+            minimum=compare(in_arr,x)
+    return minimum[-1]
+print main("/Users/sankalpyohanramesh/Google Drive/numset/testset/Impact1.png")
