c05945eaeb419ebf7264a837aa3ee95082440e96
ssannkkallpp <ssannkkallpp@gmail.com>
2015-05-01 12:58:48 +0530

    Delete list_mult.py

--- list_mult.py
+++
@@ -1,8 +0,0 @@
-def list_mult(a,b):
-    # lists a and b have to be equal
-    total=0
-    if len(a) == len(b):
-        for i in range(0,len(a)):
-            total+=a[i]*b[i]
-        return total
-    return  
