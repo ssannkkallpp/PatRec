abbf745354ad05ba935ce3c12854b77bdcc2a6a3
ssannkkallpp <ssannkkallpp@gmail.com>
2015-05-04 10:22:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import modules
 import init
 import matmult
-def main(input_file):
-    #---------------------------------------------#
-    arr=[init.init(str(i) + '.png') for i in range(1,6)]
-    arr_transpose=matmult.transpose(arr)
-    val=matmult.matmult(arr, arr_transpose)
-    #---------------------------------------------#
-    in_arr=init.init(input_file)
-    in_arr=[float(i) for i in in_arr]
-    x=[0 for i in range(len(arr)-1)]
-    x.append(1)
-    #---------------------------------------------#
-    for i in range(0,3):
-        x=matmult.matmult(val, x)
-        x=[float(m) for m in x]
-        # x is an array/vector of floats
-        total=[m**2 for m in x]
-        x=init.divide(x,sum(total))
-    #---------------------------------------------#
-    y=matmult.matmult(arr_transpose,x)
-    y=[float(i) for i in y]
-    #---------------------------------------------#
-    final=[matmult.dp(y,i) for i in arr]
-    final.append(matmult.dp(y,in_arr))
-    final_in=matmult.dp(y,in_arr)
-    return matmult.result(final, final_in)
-print main("/Users/sankalpyohanramesh/Google Drive/numset/testset/I1.png")
+import pickle
+class Init(object):
+    def __init__(self, *images):
+        self.arr=[init.init(str(i)) for i in images[0]]
+    def recognize(self, input_file):
+        arr_transpose=matmult.transpose(self.arr)
+        val=matmult.matmult(self.arr, arr_transpose)
+        in_arr=init.init(input_file)
+        in_arr=[float(i) for i in in_arr]
+        x=[0 for i in range(len(self.arr)-1)]
+        x.append(1) # x is a random unit vector
+        for i in range(0,3):
+            x=matmult.matmult(val, x)
+            x=[float(m) for m in x]
+            # x is an array/vector of floats
+            total=[m**2 for m in x]
+            try:
+                x=init.divide(x,sum(total)) # error ocurred here, so let us debug
+            except:
+                x=init.divide(x,1)
+        y=matmult.matmult(arr_transpose,x)
+        y=[float(i) for i in y]
+        final=[matmult.dp(y,i) for i in self.arr]
+        final.append(matmult.dp(y,in_arr))
+        final_in=matmult.dp(y,in_arr)
+        output=matmult.result(final, final_in)
+        return images[final.index(final[output])]
+images=["sahana2.png", "sankalp1.png"]
+test_obj=Init(images)
+print test_obj.recognize("sahana1.png")
